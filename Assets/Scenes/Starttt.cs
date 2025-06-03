85a906d760233b0e69709643c5805bdf5fe4b1a9
afnanramadhan <13521011@std.stei.itb.ac.id>
2025-06-03 12:29:38 +0700

    fix: error when change scene

--- Assets/Scenes/Starttt.cs
+++ Assets/Scenes/Starttt.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Start : MonoBehaviour
+public class Starttt : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
